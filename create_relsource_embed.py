bc19cbb6fec93a10c5d322ba0a39ec71650ebba1
Maryam <aminian.mary@gmail.com>
2018-11-23 22:20:22 -0800

    log added

--- create_relsource_embed.py
+++ create_relsource_embed.py
@@ -1,6 +1,6 @@
 from optparse import OptionParser
 from utils import *
-import codecs
+import codecs, sys
 import numpy as np
 
 if __name__ == '__main__':
@@ -28,12 +28,11 @@ if __name__ == '__main__':
     print'\nGetting relsource embeddings...'
     counter =0
     target_words_added = set()
-
+    covered = 0
     for t in wikt_dic.keys():
         counter+=1
         if counter%1000==0:
-            print str(counter)+'...'
-
+            sys.stdout.write(str(counter)+'...')
         wikt_embeddings = []
         if t in external_embedding:
             wikt_embeddings.append(external_embedding[t])
@@ -53,11 +52,13 @@ if __name__ == '__main__':
             wikt_avg = np.average(wikt_embeddings, axis=0)
             writer.write(' '.join([t] + [str(e) for e in wikt_avg]))
             writer.write('\n')
+            covered += 1
 
     for t in external_embedding.keys():
         if not t in target_words_added:
             writer.write(' '.join([t] + [str(e) for e in external_embedding[t]]))
             writer.write('\n')
-
+    sys.stdout.write('\n')
+    print str((100.0 * float(covered)/len(wikt_dic)))
     writer.flush()
     writer.close()
